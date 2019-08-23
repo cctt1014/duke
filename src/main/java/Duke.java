2b7cdfb569c5e8071981f9f43577b5086ee4b9ab
cctt1014 <e0261888@u.nus.edu>
2019-08-23 22:29:34 +0800

    implement text-ui-test and javaFx tutorial part1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,17 @@
-public class Duke {
+/**
+ * From Chen Tong.
+ * @return greeting info.
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application{
+    /**
+     * Main method.
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +19,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Goodbye World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
