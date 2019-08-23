80642f09ba2a4a7ee18457a8b30780a69cc4a771
cctt1014 <e0261888@u.nus.edu>
2019-08-23 22:40:17 +0800

    delete the javaFx GUI, current progress: text-ui-test

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,7 @@
  * @return greeting info.
  */
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-public class Duke extends Application{
+public class Duke{
     /**
      * Main method.
      */
@@ -21,11 +16,6 @@ public class Duke extends Application{
         System.out.println("Hello from\n" + logo);
     }
 
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Goodbye World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
-    }
+    
+
 }
